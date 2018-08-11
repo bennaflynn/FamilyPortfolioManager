a22f5a2c4f52f021236e62a9c4f2a5a562fc76df
Ben Flynn <bennaflynn@gmail.com>
2018-08-11 13:07:16 -0700

    Genesis Commit

---
+++ Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FamilyPortfolioManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
