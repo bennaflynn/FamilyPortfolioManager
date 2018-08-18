6bed6b67fa3bb6584791eba3de679010dd4c3a22
Ben Flynn <bennaflynn@gmail.com>
2018-08-17 19:00:18 -0700

    Adds the chart to the stock component

--- Frontend/famportman/src/Components/Home/Stocks/Stock/Stock.js
+++ Frontend/famportman/src/Components/Home/Stocks/Stock/Stock.js
@@ -3,6 +3,9 @@ import React, {Component} from 'react';
 //bootstrap
 import {Table} from 'react-bootstrap';
 
+//functional
+import {Line, Doughnut} from 'react-chartjs-2';
+
 class Stock extends Component {
     constructor(props) {
         super(props);
@@ -85,8 +88,42 @@ class Stock extends Component {
             );
         }
 
-        
+        var data = {
+            labels: dates,
+            datasets: [{
+                data: fiftyTwoWeeks,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+            
+        }
 
+        var options = {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+        
+        console.log(data, options);
         return(
             <div>
                 <h1>{name}</h1>
@@ -128,6 +165,12 @@ class Stock extends Component {
                         </tr>
                     </tbody>
                 </Table>
+
+                
+                {<Line data={data} options={options} />}
+               
+                
+                
             </div>
         );
     }
